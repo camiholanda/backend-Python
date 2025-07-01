9d06041c10dd221707ca6fef6a954fb0c148bcdd
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-01 20:59:23 -0300

    Add files via upload

---
+++ ExercicioMediaCondicional-Python-03.py
@@ -0,0 +1,14 @@
+#coments
+
+n1 = float(input("Primeiro número:"))
+n2 = float(input("Segundo número:"))
+n3 = float(input("Terceiro número:"))
+media = (n1 + n2+ n3) // 3
+
+if media >= 7:
+    print("Muito bem")
+elif media >=5 and media < 7:
+    print("Você pode mehorar")
+else:
+    print("Estude mais")
+    
