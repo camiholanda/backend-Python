d527e234b3bbb1401b7c2bc353b980a76ae1cf1b
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-01 20:43:01 -0300

    Add files via upload

---
+++ ExercicioMedia-Python-02.py
@@ -0,0 +1,10 @@
+#coments
+
+n1 = float(input("Primeira nota:"))
+n2 = float(input("Segunda nota:"))
+media = n1 + n2 // 2
+if media >= 6:
+    print("Aluno Aprovado")
+else:
+    print("Aluno Reprovado")
+    
