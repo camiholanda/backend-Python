8cee97d2c0d8b42e6e142b90ff12d21a18b3f201
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-03 19:53:43 -0300

    Add files via upload

---
+++ CalculadoraSimplesDoisNumeros.py
@@ -0,0 +1,9 @@
+n1 = int(input("Primeiro numero:"))
+n2 = int(input("Segundo numero:"))
+
+resultSoma = n1 + n2
+resultSub = n1 - n2
+resultMult = n1 * n2
+resultDiv  = n1 // n2
+
+int(print("\nResultado da soma =",resultSoma, "\nResultado da subtração = ", resultSub, "\nResultado da multiplicação = ",resultMult, "\nResultado da div = ",resultDiv))
