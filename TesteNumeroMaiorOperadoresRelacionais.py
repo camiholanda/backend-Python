694de6a8a57acb78be38dd485f91502688ed9f17
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-03 20:59:48 -0300

    Add files via upload

---
+++ TesteNumeroMaiorOperadoresRelacionais.py
@@ -0,0 +1,11 @@
+n1 = int(input("Informe número 1: "))
+n2 = int(input("Informe número 2: "))
+n3 = int(input("Informe número 3: "))
+
+if n1 > n2:
+    print("Numero", n1, "é maior")
+elif n2 > n3:
+    print("Numero", n2, "é maior")
+else: 
+     print("Numero", n3, "é maior")
+
