783400b9a225861b6f2112745030010615377ff9
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-03 20:17:13 -0300

    Add files via upload

---
+++ calculoMediaAluno.py
@@ -0,0 +1,9 @@
+n1 = float(input("Informe nota 1: "))
+n2 = float(input("Informe nota 2: "))
+
+media = n1 + n2 / 2
+
+if media >=6:
+    print("Aluno aprovado")
+else:
+    print("Aluno reprovado")
