d1ef352827b6f0df2eb568af554c7f13927e5af6
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-03 20:10:50 -0300

    Add files via upload

---
+++ calculoAreaRetangulo.py
@@ -0,0 +1,9 @@
+
+
+print("Cálculo da área de retangulo.")
+
+base = int (input("Digite o valor da base: "))
+altura = int (input("Digite o valor da altura: "))
+area = (base * altura)
+
+print("Área = ", area)
