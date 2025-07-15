c64ef7b9a0972a92e345e9b7a02cf455a6e529b3
Camila Holanda <cmdhn.holanda@gmail.com>
2025-07-15 20:42:15 -0300

    Add files via upload

---
+++ apresentarMensagemporFuncao.py
@@ -0,0 +1,21 @@
+def exibir_dados(nome, idade):
+    mensagem = (f"Olá, {nome}! Você tem {idade} anos")
+    return mensagem 
+
+nome = str(input("Nome: "))
+idad = int(input("Idade: "))
+           
+print(exibir_dados(nome, idad))
+
+
+
+
+
+
+
+
+
+
+
+
+
